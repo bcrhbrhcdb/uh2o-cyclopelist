7b5e964e7df570342601c36644f0d0b18c3f4f8b
delsix <delsixreborn@gmail.com>
2023-09-06 16:53:33 +0300

    fix for mobile

--- pages/index.js
+++ pages/index.js
@@ -8,33 +8,37 @@ export default function Home({allDemonsData}) {
     return (
         <Layout>
             <main className={styles.main}>
-                <ul className={styles.list}>
-                    {(() => {
-                        return allDemonsData.map(({id, title, author, points, difficulty}, index) => (
-                            <li className={styles.listItem} key={title}>
-                                <Link href={`/cyclope/${id}`}>
-                                    <div className={styles.cyclope}>
-                                        <div className={styles.imageContainer}>
-                                            <Image
-                                                width={90}
-                                                height={90}
-                                                src={`/images/cyclopes/${difficulty}_cyclope.png`}
-                                                alt='ЦИКЛОП!'
-                                            />
-                                        </div>
-                                        <div className={styles.mainContainer}>
-                                            <div className={styles.title}><p>{`#${++index} - ${title}`}</p></div>
-                                            <div>
-                                                <p  className={styles.author}>{author}</p>
-                                                <p  className={styles.points}>{points}<Image src="/images/cyclope-eye.png" alt="Глаз Циклопа" width={16} height={16}/></p>
+                <div className={styles.cyclopeList}>
+                    <ul className={styles.list}>
+                        {(() => {
+                            return allDemonsData.map(({id, title, author, points, difficulty}, index) => (
+                                <li className={styles.listItem} key={title}>
+                                    <Link href={`/cyclope/${id}`}>
+                                        <div className={styles.cyclope}>
+                                            <div className={styles.imageContainer}>
+                                                <Image
+                                                    width={90}
+                                                    height={90}
+                                                    src={`/images/cyclopes/${difficulty}_cyclope.png`}
+                                                    alt='ЦИКЛОП!'
+                                                />
+                                            </div>
+                                            <div className={styles.mainContainer}>
+                                                <div className={styles.title}><p>{`#${++index} - ${title}`}</p></div>
+                                                <div>
+                                                    <p className={styles.author}>{author}</p>
+                                                    <p className={styles.points}>{points}<Image
+                                                        src="/images/cyclope-eye.png" alt="Глаз Циклопа" width={16}
+                                                        height={16}/></p>
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
-                                </Link>
-                            </li>
-                        ));
-                    })()}
-                </ul>
+                                    </Link>
+                                </li>
+                            ));
+                        })()}
+                    </ul>
+                </div>
             </main>
         </Layout>
     )
